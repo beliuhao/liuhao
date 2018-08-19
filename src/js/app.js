d48848037bdeed18089b57386fcf8efae00a7897
beliuhao <contactliuhao@gmai.com>
2018-08-19 21:50:58 +0800

    1) animate the skill bars when scrolling into the skills section area.

--- src/js/app.js
+++ src/js/app.js
@@ -49,6 +49,37 @@ document.addEventListener("DOMContentLoaded", function() {
     let scrollPosition =
         document.documentElement.scrollTop || document.body.scrollTop;
     oneStepToTop.style.display = (scrollPosition <= aboutSecTopPos) ? "none" : "block";
+
+    // skill bar animation
+    let everAnimatedOnce = false;
+    const skillAnimate = function() {
+      let skills = document.querySelectorAll(".skills");
+      skills.forEach(skill => {
+        let skillStyle = window.getComputedStyle(skill);
+        let widthOfCss = skillStyle.getPropertyValue('width');
+        let keyframes = [];
+        keyframes[0] = {};
+        keyframes[0].width = 0;
+        keyframes[1] = {};
+        keyframes[1].width = widthOfCss;
+        let timeOptions = {
+          // timing options
+          easing: "ease-in-out",
+          duration: 2000,
+          iterations: 1
+        };
+        skill.animate(keyframes, timeOptions);
+      })
+    };
+
+    if (
+      scrollPosition >= (document.getElementById("skills").offsetTop + document.getElementById("skills").clientHeight * 0.2) &&
+      !everAnimatedOnce
+    ) {
+      skillAnimate();
+      everAnimatedOnce = true;
+    }
+
     // dynamically set active class to the ul#slide-out li a
     // when the corresponding section shown in the viewport
     window.onscroll = function() {
@@ -77,39 +108,25 @@ document.addEventListener("DOMContentLoaded", function() {
             .classList.add("active");
         }
       }
+      // when scrolling into the intro section area, remove the active class from sidenav
       if (scrollPosition <= sectionsInfo["about"].secTopPos * 0.5) {
         let currActive = document.querySelector(".active");
         if (currActive != null) {
           currActive.classList.remove("active");
         }
       }
-    // hide or display the #oneStepToTop div based on the current scrollPosition.
+      // hide or display the #oneStepToTop div based on the current scrollPosition.
       oneStepToTop.style.display = (scrollPosition <= aboutSecTopPos) ? "none" : "block";
 
+      // animate the skill bar when scrolling into the skills section area.
+      if (
+        scrollPosition >= (sectionsInfo["skills"].secTopPos) && !everAnimatedOnce
+      ) {
+        skillAnimate();
+        everAnimatedOnce = true;
+      }
+
     };
-
-    // skill bar animation
-    let skills = document.querySelectorAll(".skills");
-    skills.forEach(skill => {
-      let skillStyle = window.getComputedStyle(skill);
-      let widthOfCss = skillStyle.getPropertyValue('width');
-      let keyframes = [];
-      keyframes[0] = {};
-      keyframes[0].width = 0;
-      keyframes[1] = {};
-      keyframes[1].width = widthOfCss;
-      let timeOptions = {
-        // timing options
-        easing: "ease-in-out",
-        duration: 2000,
-        iterations: 1
-      };
-      skill.animate(keyframes, timeOptions);
-    })
-
-
-
-
 
 
 
