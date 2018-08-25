9f82a74f8ae474b176c841cab70a240cae5c3589
beliuhao <contactliuhao@gmai.com>
2018-08-25 11:35:18 +0800

    1) remove the dropdown component. 2) replace it with normal li.

--- src/js/app.js
+++ src/js/app.js
@@ -12,10 +12,8 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     // init the tooltip instance
     let tooltipEls = document.querySelectorAll(".tooltipped");
-    var instanceOfTooltip = M.Tooltip.init(tooltipEls, {});
-    // init the dropdown instance
-    var dropdownEls = document.querySelectorAll('.dropdown-trigger');
-    var instanceOfDropdown = M.Dropdown.init(dropdownEls, {coverTrigger: false});
+    let instanceOfTooltip = M.Tooltip.init(tooltipEls, {});
+
     // slide the carousel automaticallys
     const moveCarousel = function() {
       let currentCarousel = M.Carousel.getInstance(carouselEls[0]);
